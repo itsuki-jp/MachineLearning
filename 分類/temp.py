5b96e5294d3e33abf93b3c808ac44b87849029c1
itsuki-jp <itsuki19601@gmail.com>
2022-01-13 22:58:47 +0900

    ロジスティック回帰

---
+++ 分類/temp.py
@@ -0,0 +1,14 @@
+# setosa-versicolor.txtの最後の行を数字に変換するためだけのやつ
+reading_file = open('setosa-versicolor.txt', 'r')
+datalist = reading_file.readlines()
+data = []
+for i in datalist:
+    line = i.split(",")
+    y = 1 if line[-1] == "Iris-versicolor\n" else 0
+    temp = f'{",".join(line[:-1])},{y}\n'
+    data.append(temp)
+reading_file.close()
+
+writing_file = open("data.txt", "w")
+writing_file.writelines(data)
+writing_file.close()
---
+++ 分類/ロジスティック回帰.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# x0を加える
+def to_matrix( x ):
+    x0 = np.ones([x.shape[0], 1])
+    return np.hstack([x0, x])
+
+
+# シグモイド関数
+def f( x ):
+    return 1 / (1 + np.exp(-np.dot(x, theta)))
+
+
+# 分類関数
+def classify( x ):
+    return (f(x) >= 0.5).astype(np.int)
+
+
+# データの読み込み
+data = np.loadtxt('data.txt', delimiter=',')
+train_x = data[:, :4]
+train_y = data[:, 4]
+
+# パラメータの初期化
+theta = np.random.rand(5)
+
+X = to_matrix(train_x)
+
+ETA = 1e-3  # 学習率
+
+epoch = 5000  # 繰り返し回数
+
+for _ in range(epoch):
+    theta = theta - ETA * np.dot(f(X) - train_y, X)
+
+test = np.array([5.1, 2.5, 3.0, 1.1])
+print("versicolor" if classify(to_matrix(np.array([test]))) else "setosa")
